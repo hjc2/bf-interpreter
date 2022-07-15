3fc5907c848af7ee1c1391ed83488cf11123fd52
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2022-07-15 11:10:39 +0200

    module finished, pre clean up

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 
 from brainfuck import interpret
+import argparse
 
-code = "++++++++++[>+++++abcd++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+parser = argparse.ArgumentParser("simple_example")
+parser.add_argument("file", help="file", type=str)
+args = parser.parse_args()
+print(args.file)
+
+with open(args.file) as f:
+  code = " ".join([l.rstrip("\n") for l in f]) 
 
 output = interpret(code)
 
